3af088007dcb3fc7b59aecedcbb468726d52dd8b
ashish <tmobiletmobile14@gmail.com>
2018-04-07 00:04:17 -0400

    added index.html

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-	
-	@RequestMapping("/")
-	public String sayHello() {
-		return "index";
-	}
-
+public class IndexController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
 }
